4b1873e1627baec7fd35362f0ab69b8fb4c67ec3
sean wong <wongs28@uw.edu>
2024-11-05 16:27:23 -0800

    script can talk to quED

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify, request
 import numpy as np
 import requests
+import time
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 # Fixed IP address of the device
 DEVICE_IP = "http://128.95.31.28"  # Change this to your device's IP
@@ -12,8 +13,22 @@ vari = {
     'param': {'ild': None, 'pm1': None, 'pm2': None, 'int': None, 'cnt': None, 'mref': None}
 }
 
-if __name__ == '__main__':
-    vari['param']['pm1'] = np.arange(0, 360, 0.5)
-    print(vari['param']['pm1'])
-    # url = f":8080/?action={action}&param={param}&value={value}"
 
+def build_url(action, param, value=None):
+    if value is None:
+        return f'{DEVICE_IP}:8080/?action={action}&param={param}'
+    else:
+        return f'{DEVICE_IP}:8080/?action={action}&param={param}&value={value}'
+
+
+if __name__ == '__main__':
+    try:
+        for angle in [22.5, 67.5, 112.5, 157.5]:
+            response = requests.post(build_url('set', 'pm1', angle))
+            time.sleep(3)
+            if response.status_code == 200:
+                print(response.text)
+            else:
+                print(response.status_code)
+    except requests.exceptions.RequestException as e:
+        print(e)
