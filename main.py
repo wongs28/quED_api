aad50f152ebb041b537bc2d4c420261db7eb4730
sean wong <wongs28@uw.edu>
2024-11-12 16:32:12 -0800

    figure out S calculation

--- main.py
+++ main.py
@@ -41,25 +41,25 @@ def find_string(response, search_string):
             return previous_sibling.strip()
 
 
-def polarization_correlation():
+def polarization_correlation(pm1_angles, pm2_angles):
     cnt_rates = [[] for _ in range(4)]
     i = 0
     try:
         set_zero('pm1')
         set_zero('pm2')
-        for pm1 in [0, -45, 90, 45]:
+        time.sleep(0.1)
+        for pm1 in pm1_angles:
             set_pm1 = requests.post(build_url('set', 'pm1', pm1))
             time.sleep(0.1)
             check(set_pm1)
 
-            for pm2 in np.arange(0, 360, 10):
+            for pm2 in pm2_angles:
                 set_pm2 = requests.post(build_url('set', 'pm2', pm2))
-                time.sleep(0.1)
                 check(set_pm2)
                 cnt = requests.get(build_url('get', 'cnt'))
-                time.sleep(0.1)
+                time.sleep(0.05)
                 check(cnt)
-                cnt_rates.append(find_string(cnt, '01:')[4:])
+                cnt_rates[i].append(float(find_string(cnt, '01:')[4:]))
             i = i + 1
     except requests.exceptions.RequestException as e:
         print(e)
@@ -72,16 +72,19 @@ def calculate_E(a, a_perp, b, b_perp):
     return (a(b) - a(b_perp) - a_perp(b) + a_perp(b_perp)) / norm
 
 
-def calculate_S(h_counts, n_counts, v_counts, p_counts):
-    E_ab = calculate_E(h_counts, v_counts, 22.5, -67.5)
-    E_abp = calculate_E(h_counts, v_counts, 67.5, -22.5)
-    E_apb = calculate_E(p_counts, n_counts, 22.5, -67.5)
-    E_apbp = calculate_E(p_counts, n_counts, 67.5, -22.5)
-    return E_ab + E_abp - E_apb + E_apbp
+def calculate_S(h_counts, n_counts, v_counts, p_counts, b, bp, perp):
+    E_ab = calculate_E(h_counts, v_counts, b, b+perp)
+    E_abp = calculate_E(h_counts, v_counts, bp, bp+perp)
+    E_apb = calculate_E(p_counts, n_counts, b, b+perp)
+    E_apbp = calculate_E(p_counts, n_counts, bp, bp+perp)
+    S1 = E_ab - E_abp + E_apb + E_apbp
+    S2 = E_ab + E_abp - E_apb + E_apbp
+    S3 = E_ab - E_abp - E_apb + E_apbp
+    S4 = E_ab + E_abp + E_apb + E_apbp
+    return S1, S2, S3, S4
 
 
-def plot_curves(x_data, y_data):
-    fig, ax = plt.subplots(1, 1)
+def plot_curves(ax, x_data, y_data):
     ax.plot(x_data, y_data)
     ax.set_title('Polarization Correlation Curve')
     ax.set_xlabel('Polarizer 2 Angle')
@@ -91,16 +94,18 @@ def plot_curves(x_data, y_data):
 
 
 if __name__ == '__main__':
-    all_counts = polarization_correlation()
-    print(all_counts)
     interp = []
+    pol1_angles = [0, -45, 90, 45]
+    pol2_angles = np.arange(0, 362, 2)
+    all_counts = polarization_correlation(pol1_angles, pol2_angles)
+    fig1, ax1 = plt.subplots(1, 1)
+    fig2, ax2 = plt.subplots(1, 1)
     for data in all_counts:
-        x = np.arange(0, 360, 10)
-        plot_curves(x, data)
-        interp.append(CubicSpline(x, data))
-        plot_curves(x, CubicSpline(x, data))
+        plot_curves(ax1, pol2_angles, data)
+        x_new = np.arange(0, 360, 0.5)
+        interp.append(CubicSpline(pol2_angles, data))
+        plot_curves(ax2, x_new, CubicSpline(pol2_angles, data)(x_new))
 
-    S = calculate_S(interp[0], interp[1], interp[2], interp[3])
+    S = calculate_S(interp[0], interp[1], interp[2], interp[3], 22.5, 67.5, 90)
     print(S)
     plt.show()
-
