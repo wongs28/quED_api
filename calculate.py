5448fb80efb7f72810e258019c427d10dc89db73
sean wong <wongs28@uw.edu>
2024-11-25 11:45:05 -0800

    docstrings for main and calculate, sample data for troubleshooting

--- calculate.py
+++ calculate.py
@@ -1,39 +1,75 @@
+from main import plot_curves
+
 import csv
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import CubicSpline
 
 
 def calculate_E(a, a_perp, b, b_perp):
+    """
+    This function calculates the expectation value for angles a and b. perp = perpendicular.
+    a(b) is the coincident count rate at polarizer 1 = a and polarizer 2 = b.
+
+    :param a: cubic spline corresponding to angle a
+    :param a_perp: cubic spline corresponding to angle a_perp
+    :param b: angle b
+    :param b_perp: angle b_perp
+    :return: expectation value
+    """
     norm = a(b) + a(b_perp) + a_perp(b) + a_perp(b_perp)
     return (a(b) - a(b_perp) - a_perp(b) + a_perp(b_perp)) / norm
 
 
 def calculate_S(a, ap, a_perp, ap_perp, b, bp, perp):
+    """
+    This function calculates the S value from expectation values of a, ap, b, and bp.
+    p = prime.
+    perp = perpendicular.
+
+    :param a: cubic spline corresponding to polarizer 1 = a
+    :param ap: cubic spline corresponding to polarizer 1 = ap
+    :param a_perp: cubic spline corresponding to polarizer 1 = a_perp
+    :param ap_perp: cubic spline corresponding to polarizer 1 = ap_perp
+    :param b: polarizer 2 = angle b
+    :param bp: polarizer 2  = angle bp
+    :param perp: perpendicular angle to add to b and bp
+    :return: list of expectation values and S values
+    """
     E_ab = calculate_E(a, a_perp, b, b+perp)
     E_abp = calculate_E(a, a_perp, bp, bp+perp)
     E_apb = calculate_E(ap, ap_perp, b, b+perp)
     E_apbp = calculate_E(ap, ap_perp, bp, bp+perp)
     E = [E_ab, E_abp, E_apb, E_apbp]
+
     S1 = -E_ab + E_abp + E_apb + E_apbp
     S2 = E_ab - E_abp + E_apb + E_apbp
     S3 = E_ab + E_abp - E_apb + E_apbp
     S4 = E_ab + E_abp + E_apb - E_apbp
     S = [S1, S2, S3, S4]
+
     return E, S
 
 
 if __name__ == '__main__':
+    # open coin_data.csv and copy it to a list
     with open('coin_data.csv', 'r') as file:
         reader = csv.reader(file)
         all_data = [row for row in reader]
 
+    fig3, ax3 = plt.subplots(1, 1)
+    # create cubic splines to interpolate data for calculation
     counts = []
     for data in all_data[1:]:
-        counts.append(CubicSpline(all_data[0], data))
+        x_new = np.arange(0, 180, 0.5)
+        f_new = CubicSpline(all_data[0], data)
+        counts.append(f_new)
+        plot_curves(ax3, x_new, f_new(x_new), [0, 45, 90, 135])
 
-    S1 = calculate_S(counts[0], counts[1], counts[2], counts[3], 22.5, 67.5, -90)
-    S2 = calculate_S(counts[0], counts[1], counts[2], counts[3], 22.5, 67.5, 270)
+    S1 = calculate_S(counts[0], counts[1], counts[2], counts[3], 22.5, 67.5, 90)
+    S2 = calculate_S(counts[0], counts[1], counts[2], counts[3], 22.5, 67.5, -90)
     print(S1, S2, sep='\n')
-    # for count in counts:
-    #     for angle in [22.5, 67.5, 112.5, 157.5]:
-    #         print(count(angle))
+    for count in counts:
+        for angle in [22.5, 67.5, 112.5, 157.5]:
+            print(count(angle))
+    plt.show()
--- main.py
+++ main.py
@@ -12,6 +12,14 @@ DEVICE_IP = "http://128.95.31.28"
 
 
 def build_url(action, param, value=None):
+    """
+    This function builds the specific url to send in an http request.
+
+    :param action: str
+    :param param: str
+    :param value: int or float
+    :return: str
+    """
     if value is None:
         return f'{DEVICE_IP}:8080/?action={action}&param={param}'
     else:
@@ -19,18 +27,43 @@ def build_url(action, param, value=None):
 
 
 def set_zero(pm):
+    """
+    This function sends a POST request to set a polarizer position to zero.
+
+    :param pm: str
+    :return: http request
+    """
     return requests.post(build_url('set', pm, 0))
 
 
 def get_param(param):
+    """
+    This function sends a GET request.
+
+    :param param: str
+    :return: http request
+    """
     return requests.get(build_url('get', param))
 
 
 def set_param(param, value):
+    """
+    This function sends a POST request.
+
+    :param param:
+    :param value:
+    :return: http request
+    """
     return requests.post(build_url('set', param, value))
 
 
 def check(response):
+    """
+    This function checks if an http request was successful.
+
+    :param response: http request
+    :return: raw html
+    """
     if response.status_code == 200:
         print(response.text)
     else:
@@ -38,6 +71,13 @@ def check(response):
 
 
 def find_string(response, search_string):
+    """
+    This function searches for a specific string in html data by looping through line breaks.
+
+    :param response: http request
+    :param search_string: str
+    :return: str
+    """
     soup = BeautifulSoup(response.text, 'html.parser')
     for br_tag in soup.find_all('br'):
         previous_sibling = br_tag.find_previous_sibling(string=True)
@@ -46,24 +86,36 @@ def find_string(response, search_string):
 
 
 def polarization_correlation(pm1_angles, pm2_angles):
+    """
+    This function moves polarizers and measures coincident count rates to collect data for a polarization correation curve.
+
+    :param pm1_angles: list of polarizer 1 angles
+    :param pm2_angles: list of polarizer 2 angles
+    :return: 2D list coincidence count rates
+    """
     cnt_rates = [[] for _ in range(len(pm1_angles))]
     i = 0
     try:
+        # set both polarizers to zero
         set_zero('pm1')
         time.sleep(1.0)
         set_zero('pm2')
         time.sleep(1.0)
         for pm1 in pm1_angles:
+            # set polarizer 1
             set_pm1 = set_param('pm1', pm1)
             time.sleep(1.0)
             check(set_pm1)
 
             for pm2 in pm2_angles:
+                # set polarizer 2
                 set_pm2 = set_param('pm2', pm2)
                 check(set_pm2)
                 time.sleep(1.0)
+                # get count rates
                 cnt = get_param('cnt')
                 check(cnt)
+                # find coincidence count in html data
                 cnt_rates[i].append(float(find_string(cnt, '01:')[4:]))
             i = i + 1
     except requests.exceptions.RequestException as e:
@@ -73,6 +125,14 @@ def polarization_correlation(pm1_angles, pm2_angles):
 
 
 def plot_curves(ax, x_data, y_data, labels):
+    """
+    This function plots a polarization correlation curve
+    :param ax: ax.subplot()
+    :param x_data: polarizer 2 angles
+    :param y_data: coincident count rates
+    :param labels: polarizer 1 angles
+    :return: None
+    """
     ax.plot(x_data, y_data)
     ax.set_title('Polarization Correlation Curve')
     ax.set_xlabel('Polarizer 2 Angle')
@@ -82,18 +142,24 @@ def plot_curves(ax, x_data, y_data, labels):
 
 
 if __name__ == '__main__':
+    # set polarizer angles
     pol1_angles = [0, 45, 90, 135]
     pol2_angles = np.linspace(0, 180, 9)
+    # take polarization correlation measurements
     all_counts = polarization_correlation(pol1_angles, pol2_angles)
+    # create plot for measured data and interpolated data
     fig1, ax1 = plt.subplots(1, 1)
     fig2, ax2 = plt.subplots(1, 1)
 
     for data in all_counts:
+        # plot measured data
         plot_curves(ax1, pol2_angles, data, pol1_angles)
         x_new = np.arange(0, 180, 0.5)
         f_new = CubicSpline(pol2_angles, data)
+        # plot interpolated data
         plot_curves(ax2, x_new, f_new(x_new), pol1_angles)
 
+    # save data to coin_data.csv (first row is pol2_angles, subsequent rows are coincident count rates)
     all_data = np.vstack([pol2_angles, all_counts])
     with open('coin_data.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
